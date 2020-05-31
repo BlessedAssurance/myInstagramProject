e8d179a2bd72fb7422ae0ceb17dd63dcfb109c50
BA <engineervinceblair@gmail.com>
2020-06-01 00:51:36 +0300

    import forms from django and defined them class CreatePostForm

---
+++ instagramapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post, Comment
+
+class CreatePostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['image', 'caption']
+
+
+class CreateCommentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Comment
+        fields = ['comment']
