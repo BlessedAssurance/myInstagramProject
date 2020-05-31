7c8cd49b5e32ba7e3049a25f2322d97c9b216633
BA <engineervinceblair@gmail.com>
2020-05-31 03:06:31 +0300

    create model comment

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models. CharField(max_length=140,null=True, blank = False)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse("post_list")
