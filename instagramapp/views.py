eaed949c0a4823cf810572e44c68e6412eb835e1
BA <engineervinceblair@gmail.com>
2020-06-01 00:48:53 +0300

    edited view welcome function to class HomePageView

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,5 +1,18 @@
-from django.http  import HttpResponse
+# from django.http  import HttpResponse
+from django.urls import reverse_lazy
+from .forms import CreatePostForm, CreateCommentForm
+from .models import Post, Comment
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my Instagram Page')
+
+class HomePageView(ListView):
+    model = Comment
+    success_url = reverse_lazy('home')
+    model = Post
+    form_class= CreateCommentForm
+    template_name = 'instagramapp/index.html'
+    context_object_name = 'posts'
+    ordering =['-date_posted']
+
